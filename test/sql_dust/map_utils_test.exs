9a8856f94ae29f6b4508540476bee89e71691c7a
Daniel Willemse <daniel.willemse@holder.nl>
2016-03-22 14:18:37 +0100

    Add tests for merging with string keys

--- test/sql_dust/map_utils_test.exs
+++ test/sql_dust/map_utils_test.exs
@@ -2,6 +2,30 @@ defmodule SqlDust.MapUtilsTest do
   use ExUnit.Case
   alias SqlDust.MapUtils
 
+  test "returns atom keys regardless of merge keys" do
+    map1 = %{"a" => 4, "b" => 5, "c" => "this is c", d: nil}
+    map2 = %{"a" => 4, b: 5, c: "this is c", d: nil}
+    map3 = MapUtils.deep_merge(map1, map2)
+
+    assert map3 == %{a: 4, b: 5, c: "this is c", d: nil}
+  end
+
+  test "it can handle string keys in the original map" do
+    map1 = %{"a" => 4, "b" => 5}
+    map2 = %{a: 1, b: 2}
+    map3 = MapUtils.deep_merge(map1, map2)
+
+    assert map3 == %{a: 1, b: 2}
+  end
+
+  test "it can handle string keys in the map that is merged" do
+    map1 = %{a: 1, b: 2}
+    map2 = %{"a" => 4, "b" => 5}
+    map3 = MapUtils.deep_merge(map1, map2)
+
+    assert map3 == %{a: 4, b: 5}
+  end
+
   test "it merges values" do
     map1 = %{a: 1, b: 2}
     map2 = %{a: 4, b: 5}
@@ -64,5 +88,13 @@ defmodule SqlDust.MapUtilsTest do
     map3 = MapUtils.deep_merge(map1, map2)
 
     assert map3 == %{a: false, b: %{c: false}}
+  end
+
+  test "it merges string keys in nested maps" do
+    map1 = %{a: "3", b: %{"c" => %{d: %{ "e" => 9001 }}}}
+    map2 = %{a: "3", b: %{c: %{"d" => %{ "e" => 9001 }}}}
+    map3 = MapUtils.deep_merge(map1, map2)
+
+    assert map3 == %{a: "3", b: %{c: %{d: %{ e: 9001 }}}}
   end
 end
